469872d7d592ea55c1d92a357c0a7a1d28d37f59
Uttam Kumar Jena <145798902+uttamkj@users.noreply.github.com>
2024-10-18 17:00:08 +0530

    Add files via upload

---
+++ jq & ajax/example.js
@@ -0,0 +1,14 @@
+let std = [{
+    roll: 1,
+    name: "Ram",
+    mobile: [1111111, 2222222],
+    "email": 'ram@test.com',
+    address: {
+        city: "BBSR",
+        state: "Odisha"
+    },
+    getData: function(){}, // this is a function
+    createAt: new Date(),
+    graduated: undefined
+}, 
+{},{}]
